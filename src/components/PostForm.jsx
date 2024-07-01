25d44393c37eb1281a217c0a3bbbd45dfa2135bf
alexeiVinnichek <alexei.vinnichek@mail.ru>
2024-07-01 14:24:02 +0300

    added form universal form for working with input data and added some callbacks ffor it

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -3,19 +3,42 @@ import React from "react";
 export default class PostForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            title: '',
+        };
     }
 
     submitHandler = (event) => {
+        event.preventDefault();
+        
+        const newPost = {
+            title: this.state.title,
+            id: Date.now().toString()
+        }
+        this.setState({title: ''});
+        console.log(newPost);
+    }
 
+    changeInputHandker = (event) => {
+        this.setState((prev) => ({
+            ...prev,
+            [event.target.name]: event.target.value,
+        }))
     }
 
     render() {
         return (
             <form onSubmit={this.submitHandler}>
                 <div className="mb-3">
-                    <label for="title" class="form-label">Post header</label>
-                    <input type="text" class="form-control" id="title" />
+                    <label htmlFor="title" className="form-label">Post header</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="title" 
+                        value={this.state.title}
+                        name="title"
+                        onChange={this.changeInputHandker}
+                    />
                 </div>
                 <button className="btn btn-success" type="submit">Create</button>
             </form>
